21a0b81530d218ea7dbff9848617f35aaab01e7d
Friedemann Sommer <social@friedemannsommer.com>
2022-11-27 20:19:38 +0100

    added basic database access operations

---
+++ src/database/access.rs
@@ -0,0 +1,59 @@
+use sea_orm::EntityTrait;
+
+#[derive(thiserror::Error, Debug)]
+pub enum AccessError {
+    #[error(transparent)]
+    Database(#[from] sea_orm::DbErr),
+    #[error("Entity with ID ({0}) doesn't exist")]
+    NotFound(uuid::Uuid),
+}
+
+pub async fn get_user(
+    db: &sea_orm::DatabaseConnection,
+    user_id: uuid::Uuid,
+) -> Result<entity::user::ActiveModel, AccessError> {
+    let user_opt = entity::user::Entity::find_by_id(user_id).one(db).await?;
+
+    if let Some(user) = user_opt {
+        return Ok(user.into());
+    }
+
+    Err(AccessError::NotFound(user_id))
+}
+
+pub async fn add_user(
+    db: &sea_orm::DatabaseConnection,
+    mut user: entity::user::ActiveModel,
+) -> Result<uuid::Uuid, AccessError> {
+    user.id = sea_orm::entity::ActiveValue::Set(uuid::Uuid::new_v4());
+
+    Ok(entity::user::Entity::insert(user)
+        .exec(db)
+        .await?
+        .last_insert_id)
+}
+
+pub async fn get_tracker(
+    db: &sea_orm::DatabaseConnection,
+    user_id: uuid::Uuid,
+) -> Result<entity::tracker::ActiveModel, AccessError> {
+    let tracker_opt = entity::tracker::Entity::find_by_id(user_id).one(db).await?;
+
+    if let Some(tracker) = tracker_opt {
+        return Ok(tracker.into());
+    }
+
+    Err(AccessError::NotFound(user_id))
+}
+
+pub async fn add_tracker(
+    db: &sea_orm::DatabaseConnection,
+    mut tracker: entity::tracker::ActiveModel,
+) -> Result<uuid::Uuid, AccessError> {
+    tracker.id = sea_orm::entity::ActiveValue::Set(uuid::Uuid::new_v4());
+
+    Ok(entity::tracker::Entity::insert(tracker)
+        .exec(db)
+        .await?
+        .last_insert_id)
+}
--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,2 +1,3 @@
+pub mod access;
 pub mod connection;
 pub mod migrate;
