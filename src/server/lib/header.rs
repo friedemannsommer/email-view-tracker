c321499195ce9e52078e13065c8d47bc79fcfc07
Friedemann Sommer <social@friedemannsommer.com>
2022-12-24 03:48:44 +0100

    disabled "base-uri" via CSP restricted "form-action" to "self"

--- src/server/lib/header.rs
+++ src/server/lib/header.rs
@@ -2,7 +2,7 @@ pub fn get_default_headers_middleware() -> actix_web::middleware::DefaultHeaders
     actix_web::middleware::DefaultHeaders::new()
         .add((
             actix_web::http::header::CONTENT_SECURITY_POLICY,
-            "default-src 'none'; block-all-mixed-content; img-src data: 'self'; style-src 'self'; prefetch-src 'self'; font-src 'self'; frame-ancestors 'none'",
+            "default-src 'none'; block-all-mixed-content; img-src data: 'self'; style-src 'self'; prefetch-src 'self'; font-src 'self'; frame-ancestors 'none'; base-uri 'none'; form-action 'self'",
         ))
         .add((actix_web::http::header::REFERRER_POLICY, "no-referrer"))
         .add((actix_web::http::header::X_FRAME_OPTIONS, "SAMEORIGIN"))
