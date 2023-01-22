b480cc2ee4609421cdd706984aafe6744b479a94
Friedemann Sommer <social@friedemannsommer.com>
2023-01-22 03:40:35 +0100

    updated build script to use quote instead of string interpolation

--- build.rs
+++ build.rs
@@ -1,11 +1,16 @@
 use std::{
     env, fs,
     path::{Path, PathBuf},
+    str::FromStr,
 };
 
 use lightningcss::stylesheet::{ParserOptions, PrinterOptions, StyleSheet};
 
 const SHARED_STYLESHEET: &str = "shared.css";
+const TIME_FORMAT: &[time::format_description::FormatItem] = time::macros::format_description!(
+    "[year]-[month]-[day] [hour]:[minute]:[second] \
+    [offset_hour sign:mandatory]:[offset_minute]:[offset_second]"
+);
 
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
@@ -19,43 +24,44 @@ fn main() {
     println!("cargo:rerun-if-changed={}", asset_dir.to_str().unwrap());
 }
 
+fn generate_last_modified(datetime: time::OffsetDateTime) -> proc_macro2::TokenStream {
+    let last_modified = datetime.format(TIME_FORMAT).unwrap();
+    let last_modified_ts = proc_macro2::TokenStream::from_str(&last_modified).unwrap();
+
+    quote::quote!(time::macros::datetime!(
+        #last_modified_ts
+    ))
+}
+
 fn generate_build_asset_file(path: &Path, contents: String) -> String {
     let metadata = path.metadata().unwrap();
-    let last_modified = time::OffsetDateTime::from(metadata.modified().unwrap());
+    let file_name = path.file_name().unwrap().to_str().unwrap();
+    let file_contents = contents.as_bytes();
+    let last_modified =
+        generate_last_modified(time::OffsetDateTime::from(metadata.modified().unwrap()));
 
-    format!(
-        r#"crate::model::asset::Asset{{
-        name: "{}",
-        contents: &[{}],
-        last_modified: time::macros::datetime!({} {} UTC)
-    }}"#,
-        path.file_name().unwrap().to_str().unwrap(),
-        contents
-            .as_bytes()
-            .iter()
-            .map(|b| b.to_string())
-            .collect::<Vec<String>>()
-            .join(","),
-        last_modified.date(),
-        last_modified.time()
-    )
+    quote::quote!(crate::model::asset::Asset {
+        name: #file_name,
+        contents: &[#(#file_contents),*],
+        last_modified: #last_modified
+    })
+    .to_string()
 }
 
 fn generate_static_asset_file(path: &Path) -> String {
+    let absolute_path = fs::canonicalize(path).unwrap();
+    let absolute_path_str = absolute_path.to_str().unwrap();
+    let file_name = path.file_name().unwrap().to_str().unwrap();
     let metadata = path.metadata().unwrap();
-    let last_modified = time::OffsetDateTime::from(metadata.modified().unwrap());
+    let last_modified =
+        generate_last_modified(time::OffsetDateTime::from(metadata.modified().unwrap()));
 
-    format!(
-        r#"crate::model::asset::Asset{{
-        name: "{}",
-        contents: include_bytes!("{}"),
-        last_modified: time::macros::datetime!({} {} UTC)
-    }}"#,
-        path.file_name().unwrap().to_str().unwrap(),
-        fs::canonicalize(path).unwrap().to_str().unwrap(),
-        last_modified.date(),
-        last_modified.time()
-    )
+    quote::quote!(crate::model::asset::Asset {
+        name: #file_name,
+        contents: include_bytes!(#absolute_path_str),
+        last_modified: #last_modified
+    })
+    .to_string()
 }
 
 fn write_font_assets(out_dir: &Path, asset_dir: &Path) {
